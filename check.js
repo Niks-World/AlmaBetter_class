56965d951c561b3cf7676b0c5f04730d3be45c0b
Niks-World <nikkalyan52@gmail.com>
2023-06-29 02:24:48 +0530

    file

---
+++ check.js
@@ -0,0 +1,29 @@
+// let a=2;
+
+// console.log(a++ + ++a)
+// console.log(a++ * ++a) 
+//4+1=5 , 5+1=6
+// 4*6=24
+
+// console.log("0" == 0)
+// console.log("0" === 0)
+
+// console.log((5 > 2) && (7 < 11))
+// console.log((7 > 9) || (9 < 5))
+
+
+// if(a > 6) {
+// 	console.log("Pass")
+// } else {
+// 	console.log("Fail")
+// }
+
+// console.log(a > 6 ? "Pass" : "Fail")
+
+
+
+// var a=0
+// for(a; a<5; a++);
+// console.log(a)
+ function tail(o) {
+    for(; o.next;o=o.next) ; return o; }
