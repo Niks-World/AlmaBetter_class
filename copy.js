05abd982d3e42457360bfc75328fc35a12fe7580
Niks-World <nikkalyan52@gmail.com>
2023-06-29 04:15:24 +0530

    break

---
+++ copy.js
@@ -0,0 +1,28 @@
+// let num1 = parseFloat(prompt('Enter the first number:'));
+// let num2 = parseFloat(prompt('Enter the second number:'));
+// let operator = prompt('Enter the operator (+, -, *, /):');
+// let result;
+
+// switch (operator) {
+//   case '+':
+//     result = num1 + num2;
+//     break;
+//   case '-':
+//     result = num1 - num2;
+//     break;
+//   case '*':
+//     result = num1 * num2;
+//     break;
+//   case '/':
+//     result = num1 / num2;
+//     break;
+//   default:
+//     console.log('Invalid operator');
+// }
+
+// console.log(`The result of ${num1} ${operator} ${num2} is ${result}`);
+
+// // Output3
+
+
+
--- if_else.js
+++ if_else.js
@@ -18,6 +18,8 @@ else{
     console.log("it's Hot Outside")
 }
 
+// OUTPUT THE WATER IS WARM
+
 //Nested if...else Statement
 
 // Checks if Number is Positive or not. Also checks it the number is even or odd. 
@@ -34,12 +36,14 @@ if (num %2 == 0)
             console.log("Positive and Even")
             else
             console.log("Positvie and Odd")
+ 
+// OUTPUT NAGATIVE AND ODD            
 
 
 //  Switch Statement
 // dayOfWeek
 
-let dayofweek= 1;
+let dayofweek= 6;
 let dayname;
 
 switch(dayofweek){
@@ -77,6 +81,7 @@ switch(dayofweek){
  
 console.log(dayname);
 
+//OUTPUT SATURDAY
 
 // You can have multiple case statements that share the same code block by omitting the break statement after each case statement. 
 
@@ -107,5 +112,132 @@ switch(fruit){
 }
 
 console.log(color);
+
+//OUTPUT PURPEL
+
+//  Example Use cases of Switch Statement
+
+// 1. Type Checking in Switch Statement
+
+let value =true;
+
+switch(typeof value){
+    case "string":
+        console.log("The value is string");
+        break;
+    
+    case "number":
+        console.log("The value is number");
+        break;
+
+    case "boolean":
+        console.log("The value is boolean ")
+        break;
+
+     default:
+        console.log("the valiue is not String, Number, Boolean") 
+        break;  
+}
+
+// OUTPUT IS BOOLEAN
+
+// Simple Calculator
+// 2. Write a program to make a simple calculator with the switch statement.
+//    Use the parseFloat() function to convert the user input from a string to a number.
+
+// let nik1= parseFloat(prompt("10"));
+// let nik2= parseFloat(prompt("20"));
+// let operator = prompt('+,-,*./');
+
+// let result;
+
+// switch(operator){
+//     case "+":
+//         result = nik1 + nik2;
+//         break;
+
+//     case "-":
+//         result = nik1 - nik2;
+//         break;
+        
+//     case "*":
+//          result = nik1 * nik2;
+//          break;
+            
+//      case "/":
+//         result = nik1 / nik2;
+//          break;
+
+//     default:
+//         console.log("Invaild Operator");     
+// }
+
+// console.log(`The result of ${nik1} ${operator} ${nik2} is ${result}`);
+// expated Output is 
+//(the result of nik1 operator nik2 is result) but thise code not work
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the first number: 10', (nik1Input) => {
+  const nik1 = parseFloat(nik1Input);
+
+  rl.question('Enter the second number: 20', (nik2Input) => {
+    const nik2 = parseFloat(nik2Input);
+
+    rl.question('Enter the operator (+,-,*,/): ', (operator) => {
+      let result;
+
+      switch (operator) {
+        case "+":
+          result = nik1 + nik2;
+          break;
+        case "-":
+          result = nik1 - nik2;
+          break;
+        case "*":
+          result = nik1 * nik2;
+          break;
+        case "/":
+          result = nik1 / nik2;
+          break;
+        default:
+          console.log("Invalid Operator");
+          rl.close();
+          return;
+      }
+
+      console.log(`The result of ${nik1} ${operator} ${nik2} is ${result}`);
+      rl.close();
+    });
+  });
+});
+
+//OUTPUT UNEXPATED The result of 10 + 20 is 30
+
+
+
+// check equal or not live time =1:59
+
+// if if...else 
+
+let i = 20;
+
+i == 0 ? console.log("equal To Zero ") : console.log("Not equal")
+
+// these two way to write this same code.
+
+if (i ==0 ){
+    console.log("equal To Zero ")
+}
+else{
+    console.log("Not equal")
+}
+
+
 
 
