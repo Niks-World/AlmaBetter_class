02af5ea4cad2c97c644e898ebc3370d62dd89aa1
Niks-World <nikkalyan52@gmail.com>
2023-06-29 02:17:40 +0530

    test

---
+++ first.js
@@ -0,0 +1,61 @@
+// console.log(parseInt("123Hello"));
+// console.log(parseInt("Hello123"));
+
+// if (expression) {
+//     // code to execute if expression is true 
+//   }
+//   const num = 10;
+// if (num > 0) {
+//   console.log("The number is positive");
+// }
+// console.log(num)
+
+// program for a simple calculator
+
+// let num1 = parseFloat(prompt('100'));
+// let num2 = parseFloat(prompt('200'));
+// let operator = prompt('Enter the operator (+, -, *, /):');
+// let result;
+
+// switch (operator) {
+//   case '+':
+//     result = num1 + num2;
+//     break;
+//   case '-':
+//     result = num1 - num2;
+//     break;
+//   case '*':
+//     result = num1 * num2;
+//     break;
+//   case '/':
+//     result = num1 / num2;
+//     break;
+//   default:
+//     console.log(result);
+// }
+
+// console.log(`The result of ${num1} ${operator} ${num2} is ${result}`);
+
+// Output
+// Enter the first number: 4
+// Enter the second number: 5
+// Enter operator: +
+// The result of 4 + 5 is 9
+
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+// <div id="demo">
+//   <h2>Let AJAX change this text</h2>
+//   <button type="button" onclick="loadDoc()">Change Content</button>
+// </div>
+
+// </body>
+// </html>
+
+// this question ask on the QUIZ 
+var materials = [ 'Table', 'Chair', 'Boxes', 'Press' ]; 
+
+console.log(materials.map(material => material.length))
