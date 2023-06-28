b18455c3bbe577d5229ba606a7e13f6f4f30811e
Niks-World <nikkalyan52@gmail.com>
2023-06-29 02:37:45 +0530

    if

---
+++ if_else.js
@@ -0,0 +1,14 @@
+let temperature = 21;
+
+if (temperature <= -2){
+    console.log("it's freezing outside")
+}
+else if (temperature <=20){
+ console.log("The wateher is cool")
+}
+else if (temperature <= 30){
+    console.log("The Weather is Warm")
+}
+else{
+    console.log("it's Hot Outside")
+}
