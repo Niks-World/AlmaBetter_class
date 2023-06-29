2e13ff3f33ed9a2705cd1c650cf3d28fe41939ea
Niks-World <nikkalyan52@gmail.com>
2023-06-29 05:50:39 +0530

    quiz

--- checkfromtest.js
+++ checkfromtest.js
@@ -55,6 +55,95 @@
 // </body>
 // </html>
 
+// these all are Question of Quiz
+
 var materials = [ 'Table', 'Chair', 'Boxes', 'Press' ]; 
 
 console.log(materials.map(material => material.length))
+
+function compare() { 
+    var num=2;
+    var charb=2; 
+    if(a==b)
+     return true; else return false; }
+
+var a = Math.max();
+var b = Math.min();
+
+console.log(a);
+console.log(b);
+
+// function dog() {
+//     print("I am a dog.");
+//     }
+//     dog.sound = "Bark";
+ 
+const obj1 = {first: 20, second: 30, first: 50};
+console.log(obj1);
+
+console.log(2 !== "2");
+console.log(5 & 3);
+let x = 10; let y = "10"; console.log(x == y);
+console.log(10 & 8);
+console.log(~5);
+console.log(5 === "5");
+console.log(5 | 3);
+console.log("5" * "2");
+console.log(!true);
+
+// java
+
+function comparing() {
+    var x = 9;
+    var y = '9';
+  
+    if (x == y) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+var grade = 'E';
+var result;
+switch (grade) {
+  case 'A':
+    result += 10;
+  case 'B':
+    result += 9;
+  case 'C':
+    result += 8;
+  default:
+    result += 0;
+}
+
+console.log(result);
+
+
+var  obj=
+
+{
+
+length:20,
+
+height:35,
+
+}
+
+if('breadth' in obj === false)
+
+{
+
+obj.breadth = 12;
+
+}
+
+console.log(obj.breadth);
+
+var a = 1;
+
+if (a > 10) {
+  console.log(10);
+} else {
+  console.log(a);
+}
