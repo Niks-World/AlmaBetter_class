d9fd915f82eb8d7536eb7d527e28cfe267d8c859
Niks-World <nikkalyan52@gmail.com>
2023-06-30 21:13:07 +0530

    pop

--- JavaScript Loops & Control Statements/array.js
+++ JavaScript Loops & Control Statements/array.js
@@ -48,3 +48,41 @@ let train_name =["Rajdhani Expresss","Duronto","Channai Exprees",
 console.log(train_name.length);       //length of train is 5
 
 console.log(train_name[5-1]);     // priting the last train name (satabdi)
+ 
+// other way to get last index
+let last_index = train_name.length -1;     // - from last u can put 123
+
+ console.log(train_name[last_index]); // o/p shatabdi
+
+ // if i want to add sum number like 99
+
+ train_name.push(99,100,150);  // push will to add in value
+ 
+ console.log(train_name); //  now its add in it and print will come all togater
+
+
+// If i want to remove something from end of ant array ==> pop
+
+let arr= [1,2,3,4,5,6,7];
+
+arr.pop()               // it will remove from end (7) , its only remove from end
+arr.pop()              //  it will again remove from last(6) will remove  
+arr.pop()             // again will remove from last (5)
+console.log(arr)  
+// o/p- [1 , 2 , 3 , 4]
+
+
+// now make a empty array and add some number and remove some show what will  be out put
+
+let arr1 = [] ; // this is empty array
+
+arr1.push(12);                  // add in empty arry[12]
+arr1.push(10,10,10);           //add [12 10 10 10]
+arr1.pop();                    // -1 from last [12 10 10 ]
+arr1.push(123);                //[12 10 10 123]
+arr1.pop();                   //-1 [12 10 10 ]
+arr1.pop();                  //-1 [12 10]
+arr1.push(12,45);             // add [12 10 12 45]
+arr1.pop();                 // -1 [12 10 12]
+
+console.log(arr1);     // o/p - [12 10 12]
