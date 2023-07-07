beffb2675e212d27b19c223334659aadae3336fa
Niks-World <nikkalyan52@gmail.com>
2023-07-07 23:32:57 +0530

    obj2

---
+++ JavaScript Loops & Control Statements/Object2.js
@@ -0,0 +1,11 @@
+//     OBJECT 2
+
+let obj = {
+    name: "NIK",
+    age:24,
+    location: "Delhi"
+};
+
+let obj1={ ...obj,  // using (...) Sprade method
+Hobbies:["coding","gaming"]};
+console.log(obj1)
