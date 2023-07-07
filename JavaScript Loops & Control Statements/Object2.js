571ef682edadf6d8eb009f5c95b8a982fb97883c
Niks-World <nikkalyan52@gmail.com>
2023-07-08 03:17:47 +0530

    oj2

--- JavaScript Loops & Control Statements/Object2.js
+++ JavaScript Loops & Control Statements/Object2.js
@@ -9,3 +9,32 @@ let obj = {
 let obj1={ ...obj,  // using (...) Sprade method
 Hobbies:["coding","gaming"]};
 console.log(obj1)
+
+// GIVEN a string print, the number of times each charecter apears.
+
+let str ="Masai";
+
+let obj2={};
+
+for (i =0; i <= str.length-1; i++){
+    let key = str[i];
+    obj2[key]= 1;
+}
+console.log(obj2);
+// o/p => { M: 1, a: 1, s: 1, i: 1 } but this is wrong o/p
+
+let str1 = "Masai";
+
+let obj3 ={};
+
+for(let i =0; i<= str1.length-1; i++){
+
+    if(obj3[str1[i]] == undefined){
+        obj3[str1[i]] = 1;    //add
+    } else
+    {
+        obj3[str1[i]]++;     //incerment
+    }
+}
+console.log(obj3);
+// o/p=> { M: 1, a: 2, s: 1, i: 1 } now the right ans
