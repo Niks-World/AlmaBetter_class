47154344f219af49ecb0e49588b5088407309042
Niks-World <nikkalyan52@gmail.com>
2023-07-15 00:15:42 +0530

    ES6

---
+++ module1/app.js
@@ -0,0 +1,10 @@
+// app.js
+import { greet } from './es6.js'; // importing file
+import {sum} from "./sum.js"
+
+sum (10,20);  // calling 
+greet('Nik ');
+
+/* Output 
+30
+Hello, Nik ! */
---
+++ module1/sum.js
@@ -0,0 +1,3 @@
+export function sum(a,b){
+    console.log(a+b);
+} 
